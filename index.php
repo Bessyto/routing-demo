80f501176a2fdddb7596ac19be443bca65a30b85
btorres-miller.greenriverdev.com <btorresm@srv603.webhostingforstudents.com>
2018-01-12 14:50:00 -0500

    Added two pages

--- index.php
+++ index.php
@@ -6,13 +6,30 @@ require_once ('vendor/autoload.php');
 //Create an instance of the Base
 $f3 = Base::instance();
 
+//Set debug level (comes after we instantiate f3)
+$f3->set ('DEBUG', 3);
+
 //Define a default route
 $f3->route('GET /', function() {
     echo '<h1>Routing demo</h1>';
+}
+);
 
-//    $view = new View;
-//    echo $view->render
-//    ('view/home.html');
+//Define a page1 route
+$f3->route('GET /page1', function() {
+    echo '<h1>This is page 1</h1>';
+}
+);
+
+//Define a page1 route
+$f3->route('GET /page1/subpage-a', function() {
+    echo '<h1>This is page 1, sub page a</h1>';
+}
+);
+
+//Define a page2 route
+$f3->route('GET /page2', function() {
+    echo '<h1>This is page 2</h1>';
 }
 );
 
